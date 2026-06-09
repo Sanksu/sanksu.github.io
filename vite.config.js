0a681bae636285bf64dd1ae53318d24de4ee81a4
Sanksu <870889277@qq.com>
2026-06-09 22:49:43 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import { copyFileSync } from 'fs'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    {
+      name: 'copy-404',
+      closeBundle() {
+        copyFileSync(resolve(__dirname, 'dist/index.html'), resolve(__dirname, 'dist/404.html'))
+      }
+    }
+  ],
   base: './',
   resolve: {
     alias: {
